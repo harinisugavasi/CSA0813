0fbf0f064bd92fba49dc3e7d5027e0181caa4284
Harini <harinisugavasi56@gmail.com>
2024-07-31 14:43:29 +0530

    Create string module.py

---
+++ string module.py
@@ -0,0 +1,18 @@
+import string
+print(string.punctuation)
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+print(string.digits)
+0123456789
+print(string.hexdigits)
+0123456789abcdefABCDEF
+print(string.whitespace)
+ 	
+
+
+print(string.ascii_letters)
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+print(string.ascii_lowercase)
+abcdefghijklmnopqrstuvwxyz
+print(string.ascii_uppercase)
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
