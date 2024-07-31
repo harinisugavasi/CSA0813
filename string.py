a88f5101d4ff4b12a7395e77842a30d61540c836
Harini <harinisugavasi56@gmail.com>
2024-07-31 14:08:17 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,23 @@
+a="THIS IS ME"
+print(a.capitalize())
+This is me
+print(a.upper())
+THIS IS ME
+print(a.lower())
+this is me
+print(a.title())
+This Is Me
+print(a.swapcase())
+this is me
+print(a.split())
+['THIS', 'IS', 'ME']
+print(a.center(19,"*"))
+*****THIS IS ME****
+print(a.count('this'))
+0
+print(a.replace('THIS','THAT'))
+THAT IS ME
+b="THAT"
+a="_"
+print(a.join(b))
+T_H_A_T
